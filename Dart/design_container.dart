0aaeb9ff53fe32142b2313cd6b256d493260562c
ATRI DAS <babinsamrat123@gmail.com>
2020-10-01 13:29:29 +0530

    adds a container desgin

--- Dart/design_container.dart
+++ Dart/design_container.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main()=>runApp(MaterialApp(
   home: ContainerApp(),
-  debugShowCheckedModeBanner: false,
 ));
 
 class ContainerApp extends StatefulWidget {
